ce52ae007df370949b314b3ba13657d2c8eb7c4e
admin <admin@host.com>
2025-09-04 22:42:01 +0700

    Add KiCad config files

---
+++ config/KicadFolderLinks.bat
@@ -0,0 +1,3 @@
+mklink /J "%APPDATA%\kiCad" "D:\Github\CADLib\kicad\config\Roaming"
+mklink /J "%LOCALAPPDATA%\KiCad" "D:\Github\CADLib\kicad\config\Local"
+::mklink /J "C:\Program Files\KiCad" "D:\KiCad"
