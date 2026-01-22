b98ac8b8453b69f7bf7d0e677128faa99a077c20
admin <admin@host.com>
2026-01-22 21:46:23 +0700

    remove config folder

--- config/KicadFolderLinks.bat
+++
@@ -1,3 +0,0 @@
-mklink /J "%APPDATA%\kiCad" "D:\Github\CADLib\kicad\config\Roaming"
-mklink /J "%LOCALAPPDATA%\KiCad" "D:\Github\CADLib\kicad\config\Local"
-::mklink /J "C:\Program Files\KiCad" "D:\KiCad"
